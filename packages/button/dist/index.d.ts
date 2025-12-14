a249244715ba84b11404a7ae9eb59cfd66f659bf
DuQi <v_duqi@saicmotor.com>
2025-12-14 20:42:18 +0800

    feat(button): add basic button component

---
+++ packages/button/dist/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface ButtonProps {
+    children: React.ReactNode;
+}
+declare const Button: React.FC<ButtonProps>;
+
+export { Button, type ButtonProps, Button as default };
---
+++ packages/button/dist/index.mjs
@@ -0,0 +1,10 @@
+// src/index.tsx
+import { jsx } from "react/jsx-runtime";
+var Button = ({ children }) => {
+  return /* @__PURE__ */ jsx("button", { children });
+};
+var index_default = Button;
+export {
+  Button,
+  index_default as default
+};
